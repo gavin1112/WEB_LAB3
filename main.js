38ed6b7ad291eef3009e7417d02807213a7f7214
Gavin Chong Jia Hao <gavin021112@gmail.com>
2024-11-03 17:51:23 +0800

    Lab3_submit

---
+++ main.js
@@ -0,0 +1,128 @@
+function load2D_bg(){
+    var background = document.getElementById("background");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            
+                    
+            background.style.backgroundPositionX = xValue * 1 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
+        })
+    },false);
+
+}
+
+function load2D_bg2(){
+    var background = document.getElementById("background2");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            
+            background.style.backgroundPositionX = xValue *1 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
+        })
+    },false);
+
+}
+
+function load2D_bg2_2(){
+    var background = document.getElementById("background2");
+    document.onmousemove=function(){
+        console.log(window.outerHeight);
+        background.style.opacity=1-(getMousePos()/window.outerHeight*2.5);
+    }
+}
+
+function getMousePos(event) {
+    var e = event || window.event;
+    var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+    var y = e.pageY || e.clientY + scrollY;
+    return y;
+}
+
+function load2D_lihui(){
+    var background = document.getElementById("lihui");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var xValue = calcValue(x, window.innerWidth);
+
+            background.style.left = xValue * 0.7-100 + "px";
+            
+        })
+    },false);
+
+}
+
+function load2D_yinghua(){
+    var background = document.getElementById("yinghua");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            background.style.backgroundPositionX = xValue * 1.5 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 1-50 ) + "px";
+        })
+    },false);
+
+}
+var i=0;
+
+function campusLife(){
+    var slogan = document.getElementById("slogan");
+    var slogan2 = document.getElementById("slogan2");
+    if(i==0){
+        i=1;
+        slogan.style.height="100px";
+        slogan2.innerHTML="I'am Currently Year 3 student in Software Engineering course in UTHM. Along my university life, I meet many of cute and lovely friend. The most remembered memory during my campus life is that when I show them my Magic trick, they all expressed with it. It's nice to meet them here!^_^";
+    } else {
+        i=0;
+        slogan.style.height="50px";
+        slogan2.innerHTML="Ladies and gentlemen, it's a show time!";
+    }
+}
